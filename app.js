c6c4ab4e47593f4af95f1b9024ebca74306288da
Nadeem Hussain <hoteleyenadeem@gmail.com>
2024-10-27 11:23:56 +0500

    class code

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("while looop is running now");
+var i = 0;
+while (i <= 3) {
+  console.log(i);
+  i++;
+}
+
+console.log("do while loop is running now");
+var i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < -1);
+
+// comment in js
+// single line comment
+/* multi
+line
+comment */
